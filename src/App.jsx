5fb294e437ed5dc7886e46b5a61abc111a040a23
Samyak jain <samyakjain137@gmail.com>
2024-09-25 20:52:21 +0530

    m

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const App = () => {
   return (
     <>
-      <div>
-        <div className="page font-karla h-screen w-screen bg-customGreen flex justify-center items-center ">
+      <div className="flex justify-center items-center ">
+        <div className="page font-karla h-screen w-screen bg-customGreen">
           <div className="main-form h-[700px] w-[750px] bg-white rounded-xl">
             <div className="">
               <h2 className="text-3xl font-bold m-[30px]">Contact Us</h2>
